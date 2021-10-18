1d14b5081beade55d720c190222a3e199633b605
packetcraft <rakeshsingh005@gmail.com>
2021-10-18 22:37:09 +0530

    Update main.tf

--- external-module-example/main.tf
+++ external-module-example/main.tf
@@ -1,5 +1,5 @@
 module "dynamodb_example_complete" {
-  source = "cloudposse/terraform-aws-dynamodb/aws//examples/complete"
+  source = "cloudposse/dynamodb/aws//examples/complete"
   version = "0.29.0"
   # insert the 12 required variables here
 }
